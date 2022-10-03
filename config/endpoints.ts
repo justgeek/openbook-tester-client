874dd1f512cda1ff05f99ae3645340f6035ae9c2
mohamedtaher <mt@prollygeek.com>
2022-10-03 17:53:59 +0200

    add dotenv

--- config/endpoints.ts
+++ config/endpoints.ts
@@ -1,5 +1,5 @@
-export const OPENBOOK_API_URL = `https://api.openbook.botpress.cloud/v1`;
-export const OPENBOOK_TESTER_API_URL = `http://localhost:8080`;
+export const OPENBOOK_API_URL = process.env.OPENBOOK_API_URL;
+export const OPENBOOK_TESTER_API_URL = process.env.OPENBOOK_TESTER_API_URL;
 
 export const endPoints = {
   artifacts: `${OPENBOOK_API_URL}/artifacts`,
